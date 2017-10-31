3d9771bcfacddad8e5b68cfa699843fa79e3af6c
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2017-10-31 11:14:44 +0100

    axi4: add 512-bit data width AXI4-full record, fixed compilation issue in axi->wb bridge

--- modules/axi/axi4_pkg.vhd
+++ modules/axi/axi4_pkg.vhd
@@ -143,7 +143,7 @@ package axi4_pkg is
     function f_axi4_full_to_lite (
     f : t_axi4_full_master_out_32
     )  return t_axi4_lite_master_out_32;
-
+  
   function f_axi4_lite_to_full (
     l : t_axi4_lite_master_in_32
     )  return t_axi4_full_master_in_32;
@@ -162,7 +162,7 @@ package axi4_pkg is
     port (
       clk_sys_i : in std_logic;
       rst_n_i   : in std_logic;
-
+  
       ARVALID : in  std_logic;
       AWVALID : in  std_logic;
       BREADY  : in  std_logic;
@@ -182,14 +182,14 @@ package axi4_pkg is
       BRESP   : out std_logic_vector (1 downto 0);
       RRESP   : out std_logic_vector (1 downto 0);
       RDATA   : out std_logic_vector (31 downto 0);
-
+  
       wb_adr : out std_logic_vector(c_wishbone_address_width-1 downto 0);
       wb_dat_m2s : out std_logic_vector(c_wishbone_data_width-1 downto 0);
       wb_sel : out std_logic_vector(c_wishbone_data_width/8-1 downto 0);
       wb_cyc : out std_logic;
       wb_stb : out std_logic;
       wb_we  : out std_logic;
-
+  
       wb_dat_s2m   : in std_logic_vector(c_wishbone_data_width-1 downto 0);
       wb_err   : in std_logic := '0';
       wb_rty   : in std_logic := '0';
@@ -197,7 +197,51 @@ package axi4_pkg is
       wb_stall : in std_logic
       );
   end component;
+  
+  -- AXI4-Full interface, master output ports, 512 bits
+  type t_axi4_full_master_out_512 is record
+    ARVALID : std_logic;
+    AWVALID : std_logic;
+    BREADY  : std_logic;
+    RREADY  : std_logic;
+    WLAST   : std_logic;
+    WVALID  : std_logic;
+    ARID    : std_logic_vector (11 downto 0);
+    AWID    : std_logic_vector (11 downto 0);
+    ARBURST : std_logic_vector (1 downto 0);
+    ARLOCK  : std_logic;
+    ARSIZE  : std_logic_vector (2 downto 0);
+    AWBURST : std_logic_vector (1 downto 0);
+    AWLOCK  : std_logic;
+    AWSIZE  : std_logic_vector (2 downto 0);
+    ARPROT  : std_logic_vector (2 downto 0);
+    AWPROT  : std_logic_vector (2 downto 0);
+    ARADDR  : std_logic_vector (31 downto 0);
+    AWADDR  : std_logic_vector (31 downto 0);
+    WDATA   : std_logic_vector (511 downto 0);
+    ARCACHE : std_logic_vector (3 downto 0);
+    ARLEN   : std_logic_vector (7 downto 0);
+    ARQOS   : std_logic_vector (3 downto 0);
+    AWCACHE : std_logic_vector (3 downto 0);
+    AWLEN   : std_logic_vector (7 downto 0);
+    AWQOS   : std_logic_vector (3 downto 0);
+    WSTRB   : std_logic_vector (31 downto 0);
+  end record;
 
+  -- AXI4-Full interface, master input ports, 512 bits
+  type t_axi4_full_master_in_512 is record
+    ARREADY : std_logic;
+    AWREADY : std_logic;
+    BVALID  : std_logic;
+    RLAST   : std_logic;
+    RVALID  : std_logic;
+    WREADY  : std_logic;
+    BID     : std_logic_vector (11 downto 0);
+    RID     : std_logic_vector (11 downto 0);
+    BRESP   : std_logic_vector (1 downto 0);
+    RRESP   : std_logic_vector (1 downto 0);
+    RDATA   : std_logic_vector (511 downto 0);
+  end record;
 end package;
 
 package body axi4_pkg is
@@ -220,7 +264,7 @@ package body axi4_pkg is
     l.WSTRB   := f.WSTRB;
 
     return l;
-
+    
   end f_axi4_full_to_lite;
 
   function f_axi4_lite_to_full (
@@ -241,7 +285,7 @@ package body axi4_pkg is
     f.RDATA   := l.RDATA;
 
     return f;
-
+    
   end f_axi4_lite_to_full;
 
 
