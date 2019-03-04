16704b2c6fbe0433bb287d4e595489833987c9eb
A. Hahn <a.hahn@gsi.de>
2019-03-04 15:04:33 +0100

    network: added global_region for arria 10

---
+++ platform/altera/networks/arria10gx/global_region/altclkctrl_181/synth/global_region_pkg.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+package global_region_pkg is
+	component global_region_altclkctrl_181_cf6o3fq is
+		port (
+			inclk  : in  std_logic := 'X'; -- inclk
+			outclk : out std_logic         -- outclk
+		);
+	end component global_region_altclkctrl_181_cf6o3fq;
+
+end global_region_pkg;
