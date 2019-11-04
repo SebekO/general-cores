7138ee458c93b043382356a614af8935d73b171a
A. Hahn <a.hahn@gsi.de>
2019-11-04 14:45:19 +0100

    networks/Manifest.py: removed arria10_gx stuff

--- platform/altera/networks/Manifest.py
+++ platform/altera/networks/Manifest.py
@@ -1,6 +1,5 @@
 def __helper():
   files = [ "altera_networks_pkg.vhd" ]
-  if syn_device[:2] == "10":     files.extend(["arria10gx_networks.qip"])
   if syn_device[:1] == "5":      files.extend(["arria5_networks.qip"])
   if syn_device[:6] == "ep2agx": files.extend(["arria2gx_networks.qip"])
   return files
