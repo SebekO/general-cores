7498609f4e561511f406803abba2591a050920eb
Pascal Bos <bosp@nikhef.nl>
2020-10-07 13:17:09 +0200

    renamed pci to pcie, moved to bd format

--- modules/pcie/Manifest.py
+++ modules/pcie/Manifest.py
@@ -1,3 +1,16 @@
-modules =  { "local" : [
+modules = { "local" : [
   "pcie"
   ]}
+files = [ ];
+
+# Be sure 'target' is defined.
+try:
+        target
+except NameError:
+        target = ""
+
+# Target specific modules.
+if target == "xilinx":
+  if (syn_device[0:6].upper()=="XC7Z03" or# Family 7 (ZYNQ Z030,Z035 Z045)
+      syn_device[0:7].upper()=="XC7Z045"):
+    files.extend(["xilinx/processing_system_pcie.bd" ]);
