78156f619073858e8ef6578c5106f526dea0c044
A. Hahn <a.hahn@gsi.de>
2021-02-12 15:16:06 +0100

    platform/altera/networks: added arria10 networks to Manifest.py

--- platform/altera/networks/Manifest.py
+++ platform/altera/networks/Manifest.py
@@ -2,6 +2,9 @@ def __helper():
   files = [ "altera_networks_pkg.vhd" ]
   if syn_device[:1] == "5":      files.extend(["arria5_networks.qip"])
   if syn_device[:6] == "ep2agx": files.extend(["arria2gx_networks.qip"])
+  if syn_device[:4] == "10ax":   files.extend(["arria10gx/dual_region/dual_region.qsys", 
+                                               "arria10gx/single_region/single_region.qsys", 
+                                               "arria10gx/global_region/global_region.qsys"])
   return files
 
 files = __helper()
