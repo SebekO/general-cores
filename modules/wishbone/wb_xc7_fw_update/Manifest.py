7f6c088b7bd6912b4480618f7f3bdb48d4806d3f
David Belohrad <david@belohrad.ch>
2022-06-22 15:18:22 +0200

    conditional manifests for xilinx devices
    components purely dependent of xilinx libraries are not compiled in if
    target differs from xilinx

--- modules/wishbone/wb_xc7_fw_update/Manifest.py
+++ modules/wishbone/wb_xc7_fw_update/Manifest.py
@@ -1,3 +1,8 @@
-files = ["wb_xc7_fw_update_regs.vhd",
-         "xwb_xc7_fw_update.vhd",
-]
+import logging
+
+if target == "xilinx":
+    files = ["wb_xc7_fw_update_regs.vhd",
+             "xwb_xc7_fw_update.vhd",
+    ]
+else:
+    logging.info("Library component wb_xc7_fw_update targets only xilinx devices")
