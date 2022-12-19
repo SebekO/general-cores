368f4d1a962ee961de9eba424e3e96caee6782c3
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2022-12-19 18:21:30 +0100

    sim: separate Manifest for AXI BFMs

--- sim/Manifest.py
+++ sim/Manifest.py
@@ -1,9 +1,4 @@
 files = [ 
-"axi/axi_intf.sv",
-"axi/axi_test.sv",
-"axi/axi_pkg.sv",
-"axi/axi_utils.sv",
-"axi/rand_id_queue.sv",
 "gencores_sim_pkg.sv" ];
 
-modules = {"local" : [ "drivers" ]  };
+modules = {"local" : [ "drivers", "axi" ]  };
---
+++ sim/axi/Manifest.py
@@ -0,0 +1,5 @@
+files = [ "axi_intf.sv",
+"axi_test.sv",
+"axi_pkg.sv",
+"axi_utils.sv",
+"rand_id_queue.sv" ];
